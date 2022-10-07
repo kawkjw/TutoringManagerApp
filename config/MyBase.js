bc67f77ddfeb72219f24a00c6efe30ae9987b7de
곽진우 <jinwoo@gwagjin-uui-MacBookPro.local>
2022-10-07 15:31:36 +0900

    init

---
+++ config/MyBase.js
@@ -0,0 +1,29 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import {
+    API_KEY,
+    AUTH_DOMAIN,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_ID,
+    APP_ID,
+    MEASUREMENT_ID,
+} from "@env";
+
+const firebaseConfig = {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_ID,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID,
+};
+
+const myBase = firebase.initializeApp(firebaseConfig);
+
+export default myBase;
+export const db = myBase.firestore();
+export const arrayDelete = (d) => firebase.firestore.FieldValue.arrayRemove(d);
+export const arrayUnion = (d) => firebase.firestore.FieldValue.arrayUnion(d);
+export const fieldDelete = () => firebase.firestore.FieldValue.delete();
